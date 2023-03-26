ae451403d86f55756cff0cfc0281d1a5748f2f09
Kunal Srinivasan <98067291+srinivasan-kunal@users.noreply.github.com>
2023-03-26 09:25:35 -0400

    Created modular app.py along with the function call ins

--- Kunal/Components/test_train.py
+++ Kunal/Components/test_train.py
@@ -1,70 +1,38 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "4acef32a-93d8-474e-a1ac-9bb69d07da35",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def targetdf(inputdata):\n",
-    "    return inputdata['Signal']\n",
-    "\n",
-    "def featuresdf(inputdata):\n",
-    "    return inputdata[['symbol','close','actual_returns','ssma','ema','dema','tema','trima','sma_slow','sma_fast']].shift().dropna()\n",
-    "\n",
-    "def train_test(featuredf,targetdf):\n",
-    "    # Select the start of the training period\n",
-    "    training_begin = featuredf.index.min()\n",
-    "    # Display the training begin date\n",
-    "    # print(f'Training begins with 2 years of historical data. Starting point is: {training_begin}')\n",
-    "    # Select the ending period for the training data with an offset of 6 months\n",
-    "    training_end = featuredf.index.min() + DateOffset(months=6)\n",
-    "    # Display the training end date\n",
-    "    # print(f'Training ends with 6 months data from the start date. Training end point is: {training_end}')\n",
-    "    \n",
-    "    # Generate the X_train and y_train DataFrames\n",
-    "    X_train=featuredf.loc[training_begin:training_end]\n",
-    "    y_train=targetdf.loc[training_begin:training_end]\n",
-    "    # print(f'Testing starts a day after the training ends. Testing start point is:{training_end+DateOffset(days=1)}')\n",
-    "\n",
-    "    X_test=featuredf.loc[training_end+DateOffset(days=1):]\n",
-    "    y_test=targetdf.loc[training_end+DateOffset(days=1):]\n",
-    "    # print(f'Testing ends one day before yesterday. Testing start point is:{X_test.index.max}')\n",
-    "    return X_train, y_train, X_test, y_test\n",
-    "\n",
-    "def scaling(feature_train, feature_test, forecast_df):\n",
-    "    # Create a StandardScaler instance\n",
-    "    scaler = StandardScaler()\n",
-    "    # Apply the scaler model to fit the X-train data\n",
-    "    X_scaler = scaler.fit(feature_train)\n",
-    "    # Transform the X_train and X_test DataFrames using the X_scaler\n",
-    "    X_train_scaled = X_scaler.transform(feature_train)\n",
-    "    X_test_scaled = X_scaler.transform(feature_test)\n",
-    "    X_frcst_scaled = X_scaler.transform(forecast_df)\n",
-    "    return X_train_scaled, X_test_scaled, X_frcst_scaled"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.15"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+## This script includes functions that split the modeling dataframe into features & targets
+## `Scaling` function standardizes the feature values
+## each function returns a dataframe necessary for further processing in the main application   
+
+import datetime as dt
+from pandas.tseries.offsets import DateOffset
+#import for scaling data
+from sklearn.preprocessing import StandardScaler 
+
+def targetdf(inputdata):
+    return inputdata['Signal']
+
+def featuresdf(inputdata):
+    return inputdata[['symbol','close','actual_returns','ssma','ema','dema','tema','trima','sma_slow','sma_fast']].shift().dropna()
+
+def train_test(featuredf,targetdf):
+    # Select the start of the training period
+    training_begin = featuredf.index.min()
+    # Select the end of the training period
+    training_end = featuredf.index.min() + DateOffset(months=6)
+    # Generate the X_train and y_train DataFrames
+    X_train=featuredf.loc[training_begin:training_end]
+    y_train=targetdf.loc[training_begin:training_end]
+    # Generate the X_test and y_test DataFrames
+    X_test=featuredf.loc[training_end+DateOffset(days=1):]
+    y_test=targetdf.loc[training_end+DateOffset(days=1):]
+    return X_train, y_train, X_test, y_test
+    
+def scaling(feature_train, feature_test, forecast_df):
+    # Create a StandardScaler instance
+    scaler = StandardScaler()
+    # Apply the scaler model to fit the X-train data
+    X_scaler = scaler.fit(feature_train)
+    # Transform the X_train and X_test DataFrames using the X_scaler
+    X_train_scaled = X_scaler.transform(feature_train)
+    X_test_scaled = X_scaler.transform(feature_test)
+    X_frcst_scaled = X_scaler.transform(forecast_df)
+    return X_train_scaled, X_test_scaled, X_frcst_scaled
---
+++ Kunal/app.py
@@ -0,0 +1,194 @@
+#import for testing and training timeframes
+import datetime as dt
+from pandas.tseries.offsets import DateOffset
+
+# Import the finta Python library and the TA module
+import pandas as pd
+
+from Components.model_calls import supportvector
+from Components.model_calls import logistic
+from Components.model_calls import tree
+
+from Components.technicalind import techinds
+from Components.test_train import targetdf
+from Components.test_train import featuresdf
+from Components.test_train import train_test
+from Components.test_train import scaling
+from Components.model_select import model_selection
+from Components.expectedPerfCal import RecentPerfSummary 
+
+
+def model_iteration():
+    #Creating an empty dataframe to save the performance summary 
+    perforamance_summary=pd.DataFrame([])
+
+    for index,ticker in enumerate(tickers):
+
+        #Function call to subset the historical data with 24 months historty 
+        #and creating dataframe with technical indicators
+        tickerdf=techinds(df_portfolio_year,ticker,50,100)
+
+        #Function call for creating dataframe with target variable
+        target=targetdf(tickerdf)
+
+        #Function call for creating dataframe with feature variables
+        features=featuresdf(tickerdf)
+
+        #Creating a df with feature to forecast 'tomorrow'
+        forecastdf=pd.DataFrame(tickerdf.iloc[-1,:]).transpose()
+
+        #Splitting the features and target datasets into training and testing subsets
+        X_train, y_train, X_test, y_test=train_test(features,target)
+
+        #scaling training testing & forecast features
+        X_train_scaled, X_test_scaled, X_frcst_scaled = scaling(X_train.drop('symbol',axis=1) 
+                                                                ,X_test.drop('symbol',axis=1)
+                                                                , forecastdf.drop(['symbol','Signal']
+                                                                                  ,axis=1))
+
+        #Executing SVC model to obatin the classification report, and signal forecast for 'tomorrow'
+        svc_classification_report, method, forecast = supportvector(X_train_scaled
+                                                                    ,X_test_scaled
+                                                                    ,X_frcst_scaled
+                                                                    ,y_train
+                                                                    ,y_test)
+        result_list=list([ticker,index,method,svc_classification_report['accuracy']
+                                             ,svc_classification_report['1']['precision']
+                                             ,svc_classification_report['1']['recall']
+                                             ,svc_classification_report['-1']['precision']
+                                             ,svc_classification_report['-1']['recall']
+                                             ,forecast[0]])
+        perforamance_summary=perforamance_summary.append(pd.DataFrame([result_list]), ignore_index=True)
+        #Executing logit model to obatin the classification report, and signal forecast for 'tomorrow'
+        logit_classification_report, method ,forecast=logistic(X_train_scaled
+                                                               ,X_test_scaled
+                                                               ,X_frcst_scaled
+                                                               ,y_train
+                                                               ,y_test)
+        result_list=list([ticker,index,method,logit_classification_report['accuracy']
+                                             ,logit_classification_report['1']['precision']
+                                             ,logit_classification_report['1']['recall']
+                                             ,logit_classification_report['-1']['precision']
+                                             ,logit_classification_report['-1']['recall']
+                                             ,forecast[0]])
+        perforamance_summary=perforamance_summary.append(pd.DataFrame([result_list]), ignore_index=True)
+        #Executing Decision Tree model to obatin the classification report, and signal forecast for 'tomorrow'
+        DecTree_classification_report, method ,forecast=tree(X_train_scaled
+                                                             ,X_test_scaled
+                                                             ,X_frcst_scaled
+                                                             ,y_train
+                                                             ,y_test)
+        result_list=list([ticker,index,method,DecTree_classification_report['accuracy']
+                                             ,DecTree_classification_report['1']['precision']
+                                             ,DecTree_classification_report['1']['recall']
+                                             ,DecTree_classification_report['-1']['precision']
+                                             ,DecTree_classification_report['-1']['recall']
+                                             ,forecast[0]])
+        perforamance_summary=perforamance_summary.append(pd.DataFrame([result_list]), ignore_index=True)
+    #Updating reporting columns in the performance summary 
+    reportingcols={0:'Company',
+                   1:'Position',
+                   2:'ModelType',
+                   3:'Accuracy',
+                   4:'PrecisionForReturnIncrease',
+                   5:'RecallForReturnIncrease',
+                   6:'PrecisionForReturnDecrease',
+                   7:'RecallForReturnDecrease',
+                   8:'Forecast'}
+    perforamance_summary=perforamance_summary.rename(columns=reportingcols)
+    #Sorting the perforamnce summary for final model selection
+    final_data_sorted=perforamance_summary.sort_values(by=['Position'
+                                                           ,'Accuracy'
+                                                           ,'PrecisionForReturnIncrease'
+                                                           ,'PrecisionForReturnDecrease'
+                                                           ,'RecallForReturnIncrease'
+                                                           ,'RecallForReturnDecrease']
+                                                ,ascending=  [True
+                                                              ,False
+                                                              ,False
+                                                              ,False
+                                                              ,False
+                                                              ,False])
+
+    return final_data_sorted
+
+def PriceSummary():
+    perforamance_summary_sorted=model_iteration()
+    
+    final_summary=pd.DataFrame()
+
+    for ticker in tickers:
+        method=model_selection(ticker,perforamance_summary_sorted)
+        price, mean, stdev = RecentPerfSummary(ticker,df_portfolio_year)
+        result_list=list([ticker
+                          ,method
+                          ,price
+                          ,abs(mean)
+                          ,stdev])
+
+        final_summary=final_summary.append(pd.DataFrame([result_list]),ignore_index=True)
+
+    columns={0:'Symbol',
+             1:'PredictedSignal',
+             2:'LatestPrice',
+             3:'AvgReturns',
+             4:'StdDev'}
+
+    final_summary=final_summary.rename(columns=columns)
+
+    final_summary['expectedprice']=final_summary['LatestPrice']*(1+final_summary['PredictedSignal']*final_summary['AvgReturns'])
+    final_summary['expectedprice-high']=final_summary['LatestPrice']*(1+final_summary['PredictedSignal']*
+                                                                      (final_summary['AvgReturns']+final_summary['StdDev']))
+    final_summary['expectedprice-low']=final_summary['LatestPrice']*(1+final_summary['PredictedSignal']*
+                                                                      (final_summary['AvgReturns']-final_summary['StdDev']))
+
+    return final_summary
+
+
+# Import the required libraries and dependencies
+import os
+import requests
+import json
+import pandas as pd
+from dotenv import load_dotenv
+import alpaca_trade_api as tradeapi
+
+# Load the environment variables from the .env file
+#by calling the load_dotenv function
+load_dotenv()
+
+# Set the variables for the Alpaca API and secret keys
+alpaca_api_key=os.getenv("ALPACA_API_KEY")
+alpaca_secret_key=os.getenv("ALPACA_SECRET_KEY")
+
+# Create the Alpaca tradeapi.REST object
+alpaca=tradeapi.REST(alpaca_api_key,alpaca_secret_key,api_version="v2")
+
+# Set start and end dates of 3 years back from your current date
+# Alternatively, you can use an end date of 2020-08-07 and work 3 years back from that date 
+import datetime as dt
+from pandas.tseries.offsets import DateOffset
+
+today= dt.date.today() #- DateOffset(days=1)
+start= today - DateOffset(years=5)
+start_date=start.date()
+timeframe='1Day'
+
+tickers = ["AAPL"] #this will be gathered from sidebar eventually
+
+tickers_dfs = []
+
+timeframe = "1Day"
+
+df_portfolio_year = alpaca.get_bars(
+    tickers,
+    timeframe,
+    start = start_date
+).df
+
+#Reformatting the index
+df_portfolio_year.index=pd.to_datetime(df_portfolio_year.index).date
+
+df=PriceSummary()
+
+print(df)
