828d24fc5028c861a1efb8d6f3ba1514130585cf
baha <baha.amour@yahoo.com>
2023-03-27 21:03:28 -0400

    importing Kunal's file

--- Baha/fbprophet.py
+++ Baha/fbprophet.py
@@ -8,11 +8,21 @@ from pandas.tseries.offsets import DateOffset
 import datetime as dt
 from prophet import Prophet
 import hvplot.pandas
+import sys
+import os
 
+load_dotenv()
+
+os.chdir("../Kunal")
+sys.path.append(os.getcwd())
+from app import PriceSummary
 
 #working on ALPACA api's
 alpaca_api_key = os.getenv("ALPACA_API_KEY")
 alpaca_secret_key = os.getenv("ALPACA_SECRET_KEY")
+
+print(alpaca_secret_key)
+print(alpaca_api_key)
 
 alpaca = tradeapi.REST(
     alpaca_api_key,
@@ -44,18 +54,27 @@ df_portfolio_year = alpaca.get_bars(
 
 df_portfolio_year.index=pd.to_datetime(df_portfolio_year.index).date
 
+results_df = pd.DataFrame()
+
 # Initializing facebook prophet library
-def forecast_next_day(ticker):
-        stock_df = pd.DataFrame(df_portfolio_year.loc[df_portfolio_year["symbol"] == ticker]["close"])
+def forecast_next_day(ticker,inputdata):
+    results_df = pd.DataFrame()
+    for ticker in tickers:
+        stock_df = pd.DataFrame(inputdata.loc[inputdata["symbol"] == ticker]["close"])
         stock_df = stock_df.reset_index().rename(columns={"index": "ds", "close": "y"})
         model = Prophet()
         model.fit(stock_df)
         future = model.make_future_dataframe(periods=1, freq="D")
         forecast = model.predict(future)
         forecast_yhats= forecast[["yhat","yhat_lower","yhat_upper"]].tail(1).values[0]
-        return forecast_yhats
+        results_df = results_df.append({'ticker': ticker, 'yhat': forecast_yhats[0], 
+                                        'yhat_lower': forecast_yhats[1], 'yhat_upper': forecast_yhats[2]}, 
+                                        ignore_index=True)
+    return results_df
 
+results_df = forecast_next_day(tickers,df_portfolio_year)
 
-#Calling the def fubction
-user = input("which ticker you want to forecast?") #this will be a drop down menu 
-print(forecast_next_day(user))
+df = PriceSummary(df_portfolio_year)
+
+print(results_df)
+print(df)
