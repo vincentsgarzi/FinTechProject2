3752638b6d492f5fae1764a564a20d5b512ad530
baha <baha.amour@yahoo.com>
2023-03-29 16:44:09 -0400

    Adding comments and debuging

--- Baha/fbprophet.py
+++ Baha/fbprophet.py
@@ -3,57 +3,72 @@ import pandas as pd
 import alpaca_trade_api as tradeapi
 from prophet import Prophet
 import os
-from pathlib import Path
 from pandas.tseries.offsets import DateOffset
 import datetime as dt
-import hvplot.pandas
 import sys
 import os
 
 # Initializing facebook prophet library
+# This function uses Facebook Prophet to forecast the next day's closing price for each stock in the list of tickers.
+# It returns a DataFrame with the ticker symbol, forecasted price, and upper and lower bounds of the forecast.
+
 def forecast_next_day(tickers,inputdata):
+    # create an empty DataFrame to store the results
     results_df = pd.DataFrame()
+    # loop over each ticker symbol in the list of tickers
     for ticker in tickers:
+        # create a DataFrame with the closing prices for the current ticker
         stock_df = pd.DataFrame(inputdata.loc[inputdata["symbol"] == ticker]["close"])
+        # reset the index and rename the columns to work with Prophet
         stock_df = stock_df.reset_index().rename(columns={"index": "ds", "close": "y"})
+        # create a Prophet model and fit it to the data
         model = Prophet()
         model.fit(stock_df)
+        # create a DataFrame with the next day's date
         future = model.make_future_dataframe(periods=1, freq="D")
+        # use the model to predict the next day's closing price
         forecast = model.predict(future)
+        # extract the forecasted price and the upper and lower bounds of the forecast for the next day
         forecast_yhats= forecast[["yhat","yhat_lower","yhat_upper"]].tail(1).values[0]
+        # add the results for the current ticker to the results DataFrame
         results_df = results_df.append({'ticker': ticker, 'yhat': forecast_yhats[0],
                                         'yhat_lower': forecast_yhats[1], 'yhat_upper': forecast_yhats[2]},
                                         ignore_index=True)
-        #results_df = results_df.set_index('ticker')
+    # return the results DataFrame
     return results_df
 
 
-def compare_prices(results_df, df):
-    comp_df = pd.DataFrame(columns=["Tickers","Actual Price","Best Price","Worst Price"])
+# This function compares the forecasted prices to the actual prices and returns a DataFrame with the ticker symbol,
+# the actual price, the best-case price (the upper bound of the forecast), and the worst-case price (the lower bound
+# of the forecast).
 
+def compare_prices(results_df, df):
+    # create an empty DataFrame to store the comparison results
+    comp_df = pd.DataFrame(columns=["Tickers","Actual Price","Best Price","Worst Price"])
+    # loop over each row in the input DataFrame
     for i in range(len(df)):
+        # extract the symbol and expected price for the current row
         symbol = df.loc[i, 'Symbol']
         expected_price = df.loc[i, 'expectedprice']
         best_price = df.loc[i, 'expectedprice-high']
         worst_price = df.loc[i, 'expectedprice-low']
-        #worst_price = df.loc[i, 'expectedprice']
-
+        # loop over each row in the results DataFrame
         for j in range(len(results_df)):
+            # if the ticker symbol in the results DataFrame matches the current symbol
             if results_df.loc[j, 'ticker'] == symbol:
+                # extract the forecasted price and the upper and lower bounds of the forecast for the current ticker
                 predicted_price = results_df.loc[j, 'yhat']
                 yhat_best = results_df.loc[j, 'yhat_upper']
                 yhat_worst = results_df.loc[j, 'yhat_lower']
-
+                # compare the forecasted price to the actual price
                 if predicted_price > expected_price:
+                    # if the forecasted price is higher than the actual price, add a row to the comparison DataFrame
+                    # with the actual price, the best-case price, and the worst-case price
                     comp_df = comp_df.append({"Tickers": symbol,
                                               "Actual Price": expected_price,
                                               "Best Price": best_price,
                                               "Worst Price": worst_price}, ignore_index=True)
-                    comp_df = comp_df.set_index('Tickers')
-                else:
-                    comp_df = comp_df.append({"Tickers": symbol,
-                                              "Actual Price": predicted_price,
-                                              "Best Price": yhat_best,
-                                              "Worst Price": yhat_worst}, ignore_index=True)
+
                     comp_df = comp_df.set_index('Tickers')
     return(comp_df)
+
--- Baha/portfolio.py
+++ Baha/portfolio.py
@@ -4,42 +4,60 @@ from dotenv import load_dotenv
 import alpaca_trade_api as tradeapi
 from pandas.tseries.offsets import DateOffset
 import datetime as dt
-import hvplot.pandas
 import os
 import plotly.express as px
 
-def portfolio_returns(inputdata,tickers,weights, init_investment,selected_period):
-    closing_prices_df = pd.DataFrame()
-    for ticker in tickers:
-        closing_prices_df[ticker] = inputdata[ticker]["close"]
-    closing_prices_df.index = closing_prices_df.index
-    daily_returns = closing_prices_df.pct_change().dropna()
-    weighted_returns = daily_returns * weights
+# Define the function with input arguments: inputdata, ticker, weights, init_investment, selected_period
+def portfolio_returns(inputdata, ticker, weights, init_investment, selected_period):
     
-    # Calculate daily portfolio returns
-    inputdata["portfolio_daily_return"] = weighted_returns.sum(axis=1)
-    
-    # Calculate cumulative portfolio returns
-    inputdata["cumulative_return"] = (1 + inputdata["portfolio_daily_return"]).cumprod()
-    
-    # Calculate portfolio value
-    inputdata["portfolio_value"] = (inputdata["cumulative_return"] * init_investment)
-    
-    inputdata = inputdata["portfolio_value"]
-    
+    # Get today's date
     today = dt.date.today()
+    
+    # Calculate the start date for the given period based on the selected_period parameter
     start_date = today - DateOffset(days=selected_period)
     
+    # Slice the inputdata to get the data for the selected period
     df_period = inputdata.loc[start_date:]
-
-    # plot the portfolio value over time
-    portfolio_plot = px.line(df_period, x=df_period.index, y='portfolio_value',
-                             labels={'variable': 'Ticker', 'portfolio_value': 'Portfolio Value (USD)', 'timestamp': 'Date'})
     
-    # get the last portfolio value
-    last_value = df_period.iloc[-1]
-            
-    return portfolio_plot, last_value
+    # Create an empty dataframe to store the closing prices of the given tickers for the selected period
+    closing_prices_df = pd.DataFrame()
+    
+    # Loop through the given tickers and populate the closing_prices_df dataframe with the closing prices for each ticker
+    for ticker in tickers:
+        closing_prices_df[ticker] = df_period[ticker]["close"]
+        
+    # Set the index of the closing_prices_df dataframe to match the index of the inputdata
+    closing_prices_df.index = closing_prices_df.index
+    
+    # Calculate the daily returns for the selected period
+    daily_returns = closing_prices_df.pct_change().dropna()
+    
+    # Calculate the weighted returns for each ticker based on the given weights
+    weighted_returns = daily_returns * weights
+    
+    # Calculate the daily portfolio returns by summing the weighted returns across all tickers for each day
+    df_period["portfolio_daily_return"] = weighted_returns.sum(axis=1)
+    
+    # Calculate the cumulative portfolio returns by calculating the product of (1 + daily portfolio return) for each day
+    df_period["cumulative_return"] = (1 + df_period["portfolio_daily_return"]).cumprod()
+    
+    # Calculate the portfolio value for each day by multiplying the cumulative portfolio returns with the initial investment
+    df_period["portfolio_value"] = (df_period["cumulative_return"] * init_investment)
+    
+    # Extract the portfolio_value column from the df_period dataframe and assign it to inputdata
+    inputdata = df_period["portfolio_value"]
+    
+    # Create a line plot of the portfolio value over time using plotly express
+    portfolio_plot = px.line(inputdata, x=df_period.index, y='portfolio_value',
+                             labels={'variable': 'Ticker', 'value': 'Portfolio Value (USD)', 'timestamp': 'Date'})
+   
+    # Get the last portfolio value from the inputdata dataframe
+    last_value = inputdata.iloc[-1]
+    
+    # Return the last portfolio value and the portfolio plot
+    return last_value, portfolio_plot
+
+
 
 
    
