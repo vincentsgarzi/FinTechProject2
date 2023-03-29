0ad9df54c1d348b8e7b21c75788ad5d9d89a21c5
Kunal Srinivasan <98067291+srinivasan-kunal@users.noreply.github.com>
2023-03-28 22:35:11 -0400

    Readme updates and directory clean up

--- Kunal/Components/model_select.py
+++
@@ -1,8 +0,0 @@
-## This script used the sorted performance summary dataframe and obtains the forecast of 
-## the best performing model
-
-def model_selection(forCompany,inputdata):
-    forCompany_df=inputdata.loc[inputdata['Company']==forCompany].copy()
-    # modelselect=forCompany_df.iloc[0]['ModelType']
-    signalFrsct=forCompany_df.iloc[0]['Forecast']
-    return signalFrsct
